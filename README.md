4d9c585a31616e47607615bc34f8ae18b3ecfbd2
Paul Barrick <paul@imperativedesign.net>
2013-01-04 09:48:18 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Quick Start For Standard Project Setup
+
+
+This will be using an TDD methodology with module based JavaScript Development.
+
+
+##1) PHP Dependencies
+Make sure you have composer installed globally on your system. 
+In the root run composer install.
+
+
+##2) JS Dependencies
+Make sure you have bower installed. 
+Cd to the web folder run bower install. 
+
+Now you are ready to start coding!
---
+++ web/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Standard | Default Project Setup</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/css/styles/styles.css" rel="stylesheet">
+    </head>
+
+    <body data-api-url="">
+        <!-- fix this path -->
+        <script type="text/javascript" src="js/libs/require.js" data-main="main"></script>
+    </body>
+</html>
